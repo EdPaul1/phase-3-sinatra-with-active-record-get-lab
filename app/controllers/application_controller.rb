57286374ad564931b25bcf6650cfa2df77ec372d
EdPaul1 <saidarajab101@gmail.com>
2023-06-03 13:57:43 +0300

    get request lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
 
+  # add routes
+  get '/bakeries' do
+    #get all the bakeries from the database
+    #send them back in a JSON array
+    bakeries = Bakery.all
+
+    bakeries.to_json
+
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.order(price: :desc).all
+
+    baked_goods.to_json
+
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive_good = BakedGood.order(price: :desc).first
+    most_expensive_good.to_json
+  end
 end
